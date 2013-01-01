8f2c66d076cb95ad8262b70513d41bdba26b7a28
Michael Tang <tangmi@uw.edu>
2013-01-01 01:45:24 -0800

    this ain't perl, yo

--- Includes/StudentRND/CodeDay/Models/Team.php
+++ Includes/StudentRND/CodeDay/Models/Team.php
@@ -93,7 +93,7 @@ class Team extends \TinyDb\Orm
     {
         //make sure that all links submitted are outward facing (includes a protocol)
         $link = strtolower(trim($this->website_link));
-        if(!preg_match(/^[^:]+(?=:\/\/)/, $link)) {
+        if(!preg_match('/^[^:]+(?=:\/\/)/', $link)) {
             $link = 'http://' . $link;
         }
         return $link;
